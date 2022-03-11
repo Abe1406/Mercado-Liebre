2ade7a13f12d93bb3b82b754ba51abbc39e56e7a
abe1406 <aej.arg@gmail.com>
2022-03-11 20:52:26 -0300

    otro cambio

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-/* app.listen(3001);
-app.use(express.static(__dirname + "/public")); */
+/* app.listen(3001);*/
+
+app.use(express.static(__dirname + "/public")); 
 
 app.listen(process.env.PORT||3000,function(){
     console.log("Fuciona en puerto 3000")
 })
-
-app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/home.html"));
