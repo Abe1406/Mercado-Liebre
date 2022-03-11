0fd47cdaf8b7ec0b9425c671ece7898c0ae040ee
abe1406 <aej.arg@gmail.com>
2022-03-11 16:57:07 -0300

    "cambios final"

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.listen(3001);
-app.use(express.static(__dirname + "/public"));
+/* app.listen(3001);
+app.use(express.static(__dirname + "/public")); */
+
+app.listen(process.env.PORT||3000,function(){
+    console.log("Fuciona en puerto 3000")
+})
+web: node app.js
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/home.html"));
